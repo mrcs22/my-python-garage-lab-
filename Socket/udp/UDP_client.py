5ee860dd42cb5330f9fa7777a34e8e5a87af04c4
mrcs22 <marcusmoraes2010@hotmail.com>
2022-03-02 18:16:40 -0300

    feat: add UDP client

---
+++ Socket/udp/UDP_client.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    print("Cliente socket acriado com sucesso")
+
+    host = "localhost"
+    port = 5432
+
+    message = "Opa!"
+
+    try:
+        print("client: {}".format(message))
+        s.sendto(message.encode(), (host, port))
+
+        data, server = s.recvfrom(4096)
+        print(data.decode())
+    finally:
+        print("cliente fechando")
+        s.close()
+
+
+if(__name__ == "__main__"):
+    main()
